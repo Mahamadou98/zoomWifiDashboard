bac441df1fc715463403cdc8a7a8c0778e45179a
Mahamadou98 <it.mk2017@gmail.com>
2025-05-12 19:20:59 +0100

    vite info update

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,12 @@ import react from '@vitejs/plugin-react';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  optimizeDeps: {
-    exclude: ['lucide-react'],
+  server: {
+    host: true, // Listen on all addresses
+    port: 3000,
+  },
+  preview: {
+    host: true, // Listen on all addresses in preview mode too
+    port: 3000,
   },
 });
